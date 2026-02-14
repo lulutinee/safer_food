4df6b2711807548c7bb66c29f413caad63d84cbc
JY Lecompte <jylecompte@cintech.ca>
2026-02-14 16:50:15 -0500

    Added and tested (on dummy data) Arrhenius model (fit and predict) to ClassicalModel (ml_logic/baseline); Cleaned up Utils.ipynb; Improved streamlit frontend

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 import streamlit as st
 import requests
+import matplotlib.pyplot as plt
+from interface.inference import infer
 '''
 # SaferFood Front
 '''
+
+#Food categories to display/select from
+FOOD_CATEGORIES = ['beef', 'produce', 'seafood', 'poultry', 'pork']
+#Storage temperatures to select from
+STORAGE_TEMP_CATEGORIES = {
+    'Frozen': -18,
+    'Chilled': 4,
+    'Ambiant': 21
+}
+#Storage time span (minimum and maximum storage time allowed)
+STORAGE_TIME_SPAN = {
+    'min': 1,
+    'max': 1_000
+    }
+
+
+#INPUTS
 
 st.markdown('''
 Is it good to eat?
@@ -16,58 +35,57 @@ Is it good to eat?
 - Temperature
 - Timelength of the storage
 '''
-In_on = st.selectbox('What kinf of food?', [
-    'ground beef salad with mayonnaise', 'ground roast beef slurry',
-    'broth', 'ground beef', 'cod',
-    'infant formula hydrated with water',
-    'infant formula hydrated with milk',
-    'infant formula hydratised with apple juice.', 'BHIB',
-    'mash carrot', 'milk', 'minced chicken', 'semi-skimmed milk',
-    'chicken liver pate', 'BHI broth', 'liquid egg whites',
-    'broccoli puree', 'mushroom puree', 'potato puree',
-    'potato puree containing cysteine', 'TGYB', 'beef gravy',
-    'raw milk', 'yolk', 'MRSB', 'ham', 'water', 'beef', 'TPB',
-    'uht milk', 'all beef dog food', 'canned corn', 'chicken roll',
-    'pasteurised crabmeat', 'model frankfurter',
-    'Chicken nugget meat blend', 'fish', 'scottish pencake',
-    'madeira cake', 'non-dairy cream', 'BHI', 'chicken broth',
-    'apple juice', 'whole milk', 'skimmed milk', 'peptone solution',
-    'quality burger', 'beefburger', 'quality beefbuger',
-    'economy beefbuger', 'beef jerky', 'marinaded beef jerky',
-    'beef muscle', 'model aqueous systems based on BHIB',
-    'broccoli juice', 'potato juice', 'sweetcorn kernals (canned)',
-    'cauliflower puree', 'fresh endive', 'ground turkey',
-    'chicken breast', 'nutrient broth', 'Nutrient broth', 'TSB',
-    'half cream', 'Double cream', 'Butter', 'sausage meat mixture',
-    'fermented meat model medium', 'cooked turkey breast',
-    'cooked ham', 'cooked chicken breast', 'watermelon',
-    'sprouting alfalfa seeds', 'cottage cheese', 'raw chicken wings'])
-temperature = st.selectbox('What is the storage temperature in celcius?', [-18, 4, 20])
-time = st.number_input('Insert the number of hours between 1 and 1000')
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
 
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
+#Input food
+matrixID = st.selectbox(
+    'What do you plan on eating today ?',
+    FOOD_CATEGORIES)
+#Input temperature
+temperature = st.selectbox(
+    'How was your food stored',
+    STORAGE_TEMP_CATEGORIES.keys()
+    )
+#Input time
+time = st.slider(
+    'How long was your food stored (in hours) ?',
+    min_value = STORAGE_TIME_SPAN['min'],
+    max_value = STORAGE_TIME_SPAN['max']
+    )
 
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
+#Inputs all completed, press button to predict
+go_for_prediction = st.button(
+    'CHECK MY FOOD !'
+    )
 
-#url = 'Add url here'
+#Wrap inputs
+params = {
+    'matrixID': matrixID,
+    'temperature': temperature,
+    'time': time
+    }
 
-#params = {"In_on": In_on,
-#          "temperature": temperature,
-#          "time": time}
+if go_for_prediction:
+        st.write(
+            f"So you want to know if your {matrixID} "
+            f"that you kept at {temperature}°C "
+            f"for {time} hours is safe to eat?"
+        )
 
-#response = round(requests.get(url, params=params).json()['fare'],2)
 
-#st.markdown(f'The predicted fare is: {response} $')
-'''
+#Predict
 
-2. Let's build a dictionary containing the parameters for our API...
+results = infer(params=params)
 
-3. Let's call our API using the `requests` package...
+if results['is_safe']:
+    st.markdown('''
+                # YOUR FOOD IS SAFE TO EAT !
+                ''')
+elif results['is_safe'] == False:
+    st.markdown('''
+                # YOUR FOOD IS UNSAFE TO EAT !
+                ''')
 
-4. Let's retrieve the prediction from the **JSON** returned by the API...
-
-## Finally, we can display the prediction to the user
-'''
+st.markdown('''
+            Predicted bacterial growth
+            ''')
+st.pyplot(results['fig'])
