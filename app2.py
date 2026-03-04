7dcae8d0fb1c7abd90c04e0f675872abbe1c2baa
lulutinee <mlussier@cintech.ca>
2026-03-03 20:36:39 -0500

    Fix access to GOOGLE_APPLICATION_CREDENTIALS in app2.py

--- app2.py
+++ app2.py
@@ -19,7 +19,7 @@ from interface import recipes
 GOOGLE_CLOUD_PROJECT = st.secrets['GOOGLE_CLOUD_PROJECT']
 GOOGLE_CLOUD_LOCATION = st.secrets['GOOGLE_CLOUD_LOCATION']
 GOOGLE_GENAI_USE_VERTEXAI = st.secrets['GOOGLE_GENAI_USE_VERTEXAI']
-GOOGLE_APPLICATION_CREDENTIALS = st.secrets[gcp_service_account]
+GOOGLE_APPLICATION_CREDENTIALS = st.secrets["gcp_service_account"]['GOOGLE_APPLICATION_CREDENTIALS']
 
 # -----------------------------
 # Page Config
